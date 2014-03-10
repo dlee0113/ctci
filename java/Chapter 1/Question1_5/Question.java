1ca76c0aac696106e32a6d865010fb09bf523951
dlee0113 <dlee0113@gmail.com>
2014-03-10 09:15:30 -0400

    added print statements and comment

--- java/Chapter 1/Question1_5/Question.java
+++ java/Chapter 1/Question1_5/Question.java
@@ -13,29 +13,49 @@ public class Question {
 		return index;
 	}
 	
+	// return length of compressed string
+	// ex) abbccccccde (length: 11) -> a1b2c6d1e1 (length: 10)
 	public static int countCompression(String str) {
 		if (str == null || str.isEmpty()) return 0;
-		char last = str.charAt(0);
+		
+		char last = str.charAt(0); // keep track of processed character
 		int size = 0;
 		int count = 1;
+		
+		// start for loop from 1 since count is set to 1
 		for (int i = 1; i < str.length(); i++) {
+			// if current character matches last character, increment count by 1
+			// This condition counts number of repeated characters
 			if (str.charAt(i) == last) {
 				count++;
-			} else {
+			}
+			// if no match, current character becomes last character
+			else {
+				System.out.println("last: " + last);
+				
 				last = str.charAt(i);
+				// add 1 for a character and add length of count
+				// ex) count: 2 -> length: 1, count: 10 -> length: 2
 				size += 1 + String.valueOf(count).length();
+				
+				System.out.println("count: " + count);
+				
+				// reset count
 				count = 1;
 			} 
 		}
 		size += 1 + String.valueOf(count).length();
+
+		System.out.println("size: " + size);
+		
 		return size;
 	}
 	
 	public static String compressBad(String str) {
-		int size = countCompression(str);
-		if (size >= str.length()) {
-			return str;
-		}
+//		int size = countCompression(str);
+//		if (size >= str.length()) {
+//			return str;
+//		}
 		String mystr = "";
 		char last = str.charAt(0);
 		int count = 1;
@@ -99,9 +119,11 @@ public class Question {
 	public static void main(String[] args) {
 		String str = "abbccccccde";
 		int c = countCompression(str);
-		String str2 = compressAlternate(str);
-		String t = compressBetter("");
-		System.out.println("Compression: " + t);
+		
+		String str2 = compressBad(str);
+//		String str2 = compressAlternate(str);
+//		String t = compressBetter("");
+//		System.out.println("Compression: " + t);
 		System.out.println("Old String (len = " + str.length() + "): " + str);
 		System.out.println("New String (len = " + str2.length() + "): " + str2);
 		System.out.println("Potential Compression: " + c);
